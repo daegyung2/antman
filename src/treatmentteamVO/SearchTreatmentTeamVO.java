56a4c4dc281f8ac1c1b560cdd86e2edba7fe337c
downmemory <downmemory@192.168.1.4>
2015-12-11 23:01:58 +0900

    의료진/진료과 검색 업데이트

---
+++ src/treatmentteamVO/SearchTreatmentTeamVO.java
@@ -0,0 +1,230 @@
+package treatmentteamVO;
+
+public class SearchTreatmentTeamVO {
+    private String did;
+    private String dreg_num;
+    private String drname;
+    private String major;
+    private String depart_id;
+    private String degree1;
+    private String degree2;
+    private String degree3;
+    private String degree4;
+    private String degree5;
+    private String report1;
+    private String report2;
+    private String report3;
+    private String report4;
+    private String report5;
+    private String drimg1;
+    private String drimg2;
+    private String drimg3;
+    private String drimg4;
+    private String drimg5;
+	private String dpname;
+	private String init;
+	private String d_info1;
+	private String d_info2;
+	private String d_info3;
+	private String d_info4;
+	private String d_info5;
+	private String d_info6;
+	private String d_info7;
+	private String d_info8;
+	private String d_info9;
+	private String d_info10;
+	public String getDid() {
+		return did;
+	}
+	public void setDid(String did) {
+		this.did = did;
+	}
+	public String getDreg_num() {
+		return dreg_num;
+	}
+	public void setDreg_num(String dreg_num) {
+		this.dreg_num = dreg_num;
+	}
+	public String getDrname() {
+		return drname;
+	}
+	public void setDrname(String drname) {
+		this.drname = drname;
+	}
+	public String getMajor() {
+		return major;
+	}
+	public void setMajor(String major) {
+		this.major = major;
+	}
+	public String getDepart_id() {
+		return depart_id;
+	}
+	public void setDepart_id(String depart_id) {
+		this.depart_id = depart_id;
+	}
+	public String getDegree1() {
+		return degree1;
+	}
+	public void setDegree1(String degree1) {
+		this.degree1 = degree1;
+	}
+	public String getDegree2() {
+		return degree2;
+	}
+	public void setDegree2(String degree2) {
+		this.degree2 = degree2;
+	}
+	public String getDegree3() {
+		return degree3;
+	}
+	public void setDegree3(String degree3) {
+		this.degree3 = degree3;
+	}
+	public String getDegree4() {
+		return degree4;
+	}
+	public void setDegree4(String degree4) {
+		this.degree4 = degree4;
+	}
+	public String getDegree5() {
+		return degree5;
+	}
+	public void setDegree5(String degree5) {
+		this.degree5 = degree5;
+	}
+	public String getReport1() {
+		return report1;
+	}
+	public void setReport1(String report1) {
+		this.report1 = report1;
+	}
+	public String getReport2() {
+		return report2;
+	}
+	public void setReport2(String report2) {
+		this.report2 = report2;
+	}
+	public String getReport3() {
+		return report3;
+	}
+	public void setReport3(String report3) {
+		this.report3 = report3;
+	}
+	public String getReport4() {
+		return report4;
+	}
+	public void setReport4(String report4) {
+		this.report4 = report4;
+	}
+	public String getReport5() {
+		return report5;
+	}
+	public void setReport5(String report5) {
+		this.report5 = report5;
+	}
+	public String getDrimg1() {
+		return drimg1;
+	}
+	public void setDrimg1(String drimg1) {
+		this.drimg1 = drimg1;
+	}
+	public String getDrimg2() {
+		return drimg2;
+	}
+	public void setDrimg2(String drimg2) {
+		this.drimg2 = drimg2;
+	}
+	public String getDrimg3() {
+		return drimg3;
+	}
+	public void setDrimg3(String drimg3) {
+		this.drimg3 = drimg3;
+	}
+	public String getDrimg4() {
+		return drimg4;
+	}
+	public void setDrimg4(String drimg4) {
+		this.drimg4 = drimg4;
+	}
+	public String getDrimg5() {
+		return drimg5;
+	}
+	public void setDrimg5(String drimg5) {
+		this.drimg5 = drimg5;
+	}
+	public String getDpname() {
+		return dpname;
+	}
+	public void setDpname(String dpname) {
+		this.dpname = dpname;
+	}
+	public String getInit() {
+		return init;
+	}
+	public void setInit(String init) {
+		this.init = init;
+	}
+	public String getD_info1() {
+		return d_info1;
+	}
+	public void setD_info1(String d_info1) {
+		this.d_info1 = d_info1;
+	}
+	public String getD_info2() {
+		return d_info2;
+	}
+	public void setD_info2(String d_info2) {
+		this.d_info2 = d_info2;
+	}
+	public String getD_info3() {
+		return d_info3;
+	}
+	public void setD_info3(String d_info3) {
+		this.d_info3 = d_info3;
+	}
+	public String getD_info4() {
+		return d_info4;
+	}
+	public void setD_info4(String d_info4) {
+		this.d_info4 = d_info4;
+	}
+	public String getD_info5() {
+		return d_info5;
+	}
+	public void setD_info5(String d_info5) {
+		this.d_info5 = d_info5;
+	}
+	public String getD_info6() {
+		return d_info6;
+	}
+	public void setD_info6(String d_info6) {
+		this.d_info6 = d_info6;
+	}
+	public String getD_info7() {
+		return d_info7;
+	}
+	public void setD_info7(String d_info7) {
+		this.d_info7 = d_info7;
+	}
+	public String getD_info8() {
+		return d_info8;
+	}
+	public void setD_info8(String d_info8) {
+		this.d_info8 = d_info8;
+	}
+	public String getD_info9() {
+		return d_info9;
+	}
+	public void setD_info9(String d_info9) {
+		this.d_info9 = d_info9;
+	}
+	public String getD_info10() {
+		return d_info10;
+	}
+	public void setD_info10(String d_info10) {
+		this.d_info10 = d_info10;
+	}
+	
+	
+}
