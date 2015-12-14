d25eb18ce70aae130ee3a32c304abe378cf75f5f
cheondaegyung <shgkdl118@gmail.com>
2015-12-14 12:22:10 +0900

    commit and update doctor insert and list file

--- src/admin/doctor/InsertDoctorProBean.java
+++ src/admin/doctor/InsertDoctorProBean.java
@@ -2,10 +2,14 @@ package admin.doctor;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.orm.ibatis.SqlMapClientTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
@@ -16,21 +20,27 @@ import admin.bean.DoctorDTO;
 public class InsertDoctorProBean {
 	@Autowired
 	private SqlMapClientTemplate sqlMapClient;
+	private List fileList;
 	
 	@RequestMapping("/insertDoctorPro.do")
-	 public String fileUpload(MultipartHttpServletRequest request,DoctorDTO dto) throws IllegalStateException, IOException{
-		 for(int i = 1; i <= 5 ; i++){
-		   MultipartFile file = request.getFile("drimg"+i);
+	 public String fileUpload(MultipartHttpServletRequest request,@ModelAttribute DoctorDTO dto,HttpServletRequest request2) throws IllegalStateException, IOException{
+		   sqlMapClient.insert("doctor.insertDr", dto);
+		   fileList = request.getFiles("drimg");
+		   for(int i = 0; i < 5 ; i++){
+		   MultipartFile file = (MultipartFile) fileList.get(i);
 		   if(!file.isEmpty()){
 		   String name = file.getOriginalFilename();
-		   File sf = new File("e://save//"+name);
+		   File sf = new File("H://antman//WebContent//image//"+name);
 		   file.transferTo(sf);
-		   request.setAttribute("drimg"+i,"e://save//"+name);
-			 }else{
-			 i = 5;
-			 }
-		 sqlMapClient.insert("doctor.insertDr", dto);
-		 }
+		   dto.setDrimg1("/antman/WebContent/image/"+name);
+		   sqlMapClient.update("doctor.updateDrimg"+(i+1),dto);
+		   }else{
+		   dto.setDrimg1("");
+		   sqlMapClient.update("doctor.updateDrimg"+(i+1),dto);
+		   }
+		   }
+	
+		 
 		 return "doctorList.do";
 	 }
 
