7fffa1064fdbf91bb498467286f1d8548d6261a8
songsunho <downmemory@naver.com>
2015-12-24 13:54:36 +0900

    대경아 돌아와 형이 더 지릴께.....

--- src/appointment/AppointmentDetailSearch.java
+++ src/appointment/AppointmentDetailSearch.java
@@ -1,6 +1,7 @@
 package appointment;
 
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -40,29 +41,47 @@ public class AppointmentDetailSearch {
 			 dpname = no;
 		}
 		
-		List<ScheduleDTO> adlist = sqlMapClient.queryForList("appointment.appointscheduleselect", dto.getDrid());
-		List<ScheduleDTO> sdlist = sqlMapClient.queryForList("schedule.scheduleselect", dto.getDrid());
+		List<AppointmentDTO> adlist = sqlMapClient.queryForList("appointment.appointscheduleselect", dto);
+		List<ScheduleDTO> sdlist = sqlMapClient.queryForList("schedule.scheduleselect", sddto);
+		List<ScheduleDTO> temp = sqlMapClient.queryForList("schedule.scheduleselect", sddto);
+		
 		System.out.println(adlist.size());
 		System.out.println(sdlist.size());
 		
-		for(ScheduleDTO aa : adlist){
-			if(sdlist.contains(aa)){
-				sdlist.remove(aa);
+		int i = 0;
+		
+		for(ScheduleDTO aa : temp){
+			for(AppointmentDTO bb : adlist) {
+				if((aa.getSdate().equals(bb.getAdate()))) {
+					sdlist.remove(i);
+					i--;
+				}			
 			}
+			i++;
 		}
-		 
-		 request.setAttribute("dpname",dpname);
-		 request.setAttribute("drname",drname);
-		 request.setAttribute("jumin1",jumin1);
-		 request.setAttribute("jumin2",jumin2);
-		 request.setAttribute("sdate",sdate);
-		 request.setAttribute("name",name);
-		 request.setAttribute("tmsdto", tmsdto);
-		 request.setAttribute("tmslist", tmslist);
-		 request.setAttribute("sdlist", sdlist);
-		 request.setAttribute("drid",drid);
+		
+		/*for(int i=0; i < sdlist.size(); i++) {
+			System.out.println(sdlist.get(i));
+			for(int j=0; j < adlist.size(); j++) {
+				if(sdlist.get(i) == ((ScheduleDTO) adlist.get(j))) {
+					System.out.println("abc");
+					sdlist.remove(i);
+				}
+			}
+		}*/
+		
+		request.setAttribute("dpname",dpname);
+		request.setAttribute("drname",drname);
+		request.setAttribute("jumin1",jumin1);
+		request.setAttribute("jumin2",jumin2);
+		request.setAttribute("sdate",sdate);
+		request.setAttribute("name",name);
+		request.setAttribute("tmsdto", tmsdto);
+		request.setAttribute("tmslist", tmslist);
+		request.setAttribute("sdlist", sdlist);
+		request.setAttribute("drid",drid);
 	
-		 return "/appointmentdetail.do";
+		return "/appointmentdetail.do";
 	}
 	
 }
